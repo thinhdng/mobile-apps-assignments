2b5c48d7c780faf261b17b5801f8069cb353ce80
thinhdng <thinh.d.nguyen1341@gmail.com>
2023-11-29 17:51:04 -0600

    display the reviews when pressing on the buttons with hero effects

--- project_2/lib/createreview.dart
+++ project_2/lib/createreview.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'booktitle.dart';
 import 'bookauthor.dart';
@@ -6,6 +8,9 @@ import 'reviewbody.dart';
 import 'reviewtitle.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:uuid/uuid.dart';
+
+bool isVisible = false;
 
 class CreateNewReviewPage extends StatelessWidget {
   const CreateNewReviewPage({super.key});
@@ -47,7 +52,8 @@ class _SubmitButtonImplementation extends State<SubmitButton> {
                 BookAuthorInput.mycontroller.text.toString(),
                 ReviewAuthorInput.mycontroller.text.toString(),
                 ReviewTitleInput.mycontroller.text.toString(),
-                ReviewBodyInput.mycontroller.text.toString()),
+                ReviewBodyInput.mycontroller.text.toString(),
+                context),
             style: TextButton.styleFrom(
                 foregroundColor: Colors.red,
                 textStyle: const TextStyle(fontSize: 20)),
@@ -56,60 +62,50 @@ class _SubmitButtonImplementation extends State<SubmitButton> {
     );
   }
 
-  Future<Review> submitReview(String title, String author, String reviewer,
-      String reviewtitle, String reviewtext) async {
+  Future<dynamic> submitReview(String title, String author, String reviewer,
+      String reviewtitle, String reviewtext, BuildContext context) async {
     final response =
         await http.post(Uri.parse('https://api.restpoint.io/api/book'),
             headers: <String, String>{
               'Content-Type': 'application/json',
-              'x-endpoint-key': '84afd7c94c40453bacee7e66833638d5'
+              'x-endpoint-key': '84afd7c94c40453bacee7e66833638d5',
+              'accept': 'application/json'
             },
             body: jsonEncode(<String, String>{
+              "id": const Uuid().v8(),
               "title": title,
               "author": author,
               "reviewer": reviewer,
               "reviewtitle": reviewtitle,
               "reviewtext": reviewtext
             }));
-
-    if (response.statusCode == 201) {
-      return Review.fromJson(jsonDecode(response.body) as Map<String, String>);
-    } else {
-      throw Exception('Failed to create review');
+    if (response.statusCode == 200) {
+      BookTitleInput.mycontroller.clear();
+      BookAuthorInput.mycontroller.clear();
+      ReviewAuthorInput.mycontroller.clear();
+      ReviewTitleInput.mycontroller.clear();
+      ReviewBodyInput.mycontroller.clear();
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: const Text(
+              'Review created successfully!',
+            ),
+            actions: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
     }
-  }
-}
-
-class Review {
-  final String bookauthor;
-  final String booktitle;
-  final String reviewauthor;
-  final String reviewbody;
-  final String reviewtitle;
-  const Review(
-      {required this.booktitle,
-      required this.bookauthor,
-      required this.reviewauthor,
-      required this.reviewtitle,
-      required this.reviewbody});
-
-  factory Review.fromJson(Map<String, String> json) {
-    return switch (json) {
-      {
-        "title": String booktitle,
-        "author": String bookauthor,
-        "reviewer": String reviewauthor,
-        "reviewtitle": String reviewtitle,
-        "reviewtext": String reviewbody
-      } =>
-        Review(
-          booktitle: booktitle,
-          bookauthor: bookauthor,
-          reviewauthor: reviewauthor,
-          reviewtitle: reviewtitle,
-          reviewbody: reviewbody,
-        ),
-      _ => throw const FormatException('Failed to load review')
-    };
   }
 }
--- project_2/lib/main.dart
+++ project_2/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'createreview.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -27,7 +30,71 @@ class MainApp extends StatelessWidget {
                                     const CreateNewReviewPage()));
                       },
                       child: const Icon(Icons.create)),
-                  body: const Text('add list of reviews here'),
+                  body: const DisplayReviews(),
                 )));
+  }
+}
+
+class DisplayReviews extends StatefulWidget {
+  const DisplayReviews({super.key});
+  @override
+  State<DisplayReviews> createState() => _DisplayReviewImplementation();
+}
+
+class _DisplayReviewImplementation extends State<DisplayReviews> {
+  static List<Review> _reviewList = List<Review>.empty();
+  Future<void> fetchReview() async {
+    final response = await http
+        .get(Uri.parse('https://api.restpoint.io/api/book'), headers: {
+      "x-endpoint-key": "84afd7c94c40453bacee7e66833638d5",
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    });
+    final List<Review> reviewList = (json.decode(response.body)['data'] as List)
+        .map((data) => Review.fromJson(data))
+        .toList();
+
+    setState(() => _reviewList = reviewList);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    fetchReview();
+    return ListView.builder(
+        itemCount: _reviewList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+              padding: const EdgeInsets.all(16),
+            ),
+            child: Text('Review of: ${_reviewList[index].title}'),
+          );
+        });
+  }
+}
+
+class Review {
+  final String title;
+  final String author;
+  final String reviewer;
+  final String reviewtitle;
+  final String reviewtext;
+  Review({
+    required this.title,
+    required this.author,
+    required this.reviewer,
+    required this.reviewtitle,
+    required this.reviewtext,
+  });
+
+  factory Review.fromJson(Map<String, dynamic> json) {
+    return Review(
+        title: json['title'],
+        author: json['author'],
+        reviewer: json['reviewer'],
+        reviewtitle: json['reviewtitle'],
+        reviewtext: json['reviewtext']);
   }
 }
